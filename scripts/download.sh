05b8169e6052016bc1f39eb529f4ce04ba8ef5d4
Sunoru <s@sunoru.com>
2023-07-06 20:15:11 -0400

    Update scripts.

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "NODE_VERSION=${NODE_VERSION}"
-echo "NODE_SOURCE_DIR=${NODE_SOURCE_DIR}"
 curl -O https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}.tar.gz
 tar zxf ./node-${NODE_VERSION}.tar.gz
 mv ./node-${NODE_VERSION} ./node
