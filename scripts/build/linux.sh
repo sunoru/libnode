bf6bcf4cd16f9b78e8b27b1815e89cf883f317d1
Sunoru <s@sunoru.com>
2023-07-06 20:18:28 -0400

    Update scripts.

--- scripts/build/linux.sh
+++ scripts/build/linux.sh
@@ -8,14 +8,14 @@ else
     if [[ "$TARGET_ARCH" == "armv7" ]]; then
         DEST_CPU="arm"
         sudo apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-        CC=arm-linux-gnueabihf-gcc
-        CXX=arm-linux-gnueabihf-g++
+        export CC=arm-linux-gnueabihf-gcc
+        export CXX=arm-linux-gnueabihf-g++
     elif [[ "$TARGET_ARCH" == "x86" ]]; then
         sudo apt-get install gcc-multilib g++-multilib
     elif [[ "$TARGET_ARCH" == "arm64" ]]; then
         sudo apt-get install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
-        CC=aarch64-linux-gnu-gcc
-        CXX=aarch64-linux-gnu-g++
+        export CC=aarch64-linux-gnu-gcc
+        export CXX=aarch64-linux-gnu-g++
     fi
     patch -p1 < $WORKSPACE/patches/configure.py.patch
     ./configure --prefix=$OUTPUT_PREFIX --shared \
